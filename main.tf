eadcf4f0e5e65791a685a226ba309e3a06c2cc95
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:36 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+data "azurerm_key_vault_secret" "client_secret" {
+  for_each     = { for k, v in var.api_management_authorization_servers : k => v if v.client_secret_key_vault_id != null && v.client_secret_key_vault_secret_name != null }
+  name         = each.value.client_secret_key_vault_secret_name
+  key_vault_id = each.value.client_secret_key_vault_id
+}
+data "azurerm_key_vault_secret" "resource_owner_password" {
+  for_each     = { for k, v in var.api_management_authorization_servers : k => v if v.resource_owner_password_key_vault_id != null && v.resource_owner_password_key_vault_secret_name != null }
+  name         = each.value.resource_owner_password_key_vault_secret_name
+  key_vault_id = each.value.resource_owner_password_key_vault_id
+}
 resource "azurerm_api_management_authorization_server" "api_management_authorization_servers" {
   for_each = var.api_management_authorization_servers
 
@@ -13,10 +23,10 @@ resource "azurerm_api_management_authorization_server" "api_management_authoriza
   support_state                = each.value.support_state
   bearer_token_sending_methods = each.value.bearer_token_sending_methods
   client_authentication_method = each.value.client_authentication_method
-  client_secret                = each.value.client_secret
+  client_secret                = each.value.client_secret != null ? each.value.client_secret : try(data.azurerm_key_vault_secret.client_secret[each.key].value, null)
   default_scope                = each.value.default_scope
   description                  = each.value.description
-  resource_owner_password      = each.value.resource_owner_password
+  resource_owner_password      = each.value.resource_owner_password != null ? each.value.resource_owner_password : try(data.azurerm_key_vault_secret.resource_owner_password[each.key].value, null)
   resource_owner_username      = each.value.resource_owner_username
   token_endpoint               = each.value.token_endpoint
 
--- variables.tf
+++ variables.tf
@@ -15,9 +15,13 @@ Optional:
     - bearer_token_sending_methods
     - client_authentication_method
     - client_secret
+    - client_secret_key_vault_id (alternative to client_secret - read from Key Vault instead)
+    - client_secret_key_vault_secret_name (alternative to client_secret - read from Key Vault instead)
     - default_scope
     - description
     - resource_owner_password
+    - resource_owner_password_key_vault_id (alternative to resource_owner_password - read from Key Vault instead)
+    - resource_owner_password_key_vault_secret_name (alternative to resource_owner_password - read from Key Vault instead)
     - resource_owner_username
     - support_state
     - token_endpoint
@@ -27,24 +31,28 @@ Optional:
 EOT
 
   type = map(object({
-    api_management_name          = string
-    name                         = string
-    grant_types                  = set(string)
-    display_name                 = string
-    resource_group_name          = string
-    client_id                    = string
-    authorization_methods        = set(string)
-    authorization_endpoint       = string
-    client_registration_endpoint = string
-    support_state                = optional(bool)
-    bearer_token_sending_methods = optional(set(string))
-    client_authentication_method = optional(set(string))
-    client_secret                = optional(string)
-    default_scope                = optional(string)
-    description                  = optional(string)
-    resource_owner_password      = optional(string)
-    resource_owner_username      = optional(string)
-    token_endpoint               = optional(string)
+    api_management_name                           = string
+    name                                          = string
+    grant_types                                   = set(string)
+    display_name                                  = string
+    resource_group_name                           = string
+    client_id                                     = string
+    authorization_methods                         = set(string)
+    authorization_endpoint                        = string
+    client_registration_endpoint                  = string
+    support_state                                 = optional(bool)
+    bearer_token_sending_methods                  = optional(set(string))
+    client_authentication_method                  = optional(set(string))
+    client_secret                                 = optional(string)
+    client_secret_key_vault_id                    = optional(string)
+    client_secret_key_vault_secret_name           = optional(string)
+    default_scope                                 = optional(string)
+    description                                   = optional(string)
+    resource_owner_password                       = optional(string)
+    resource_owner_password_key_vault_id          = optional(string)
+    resource_owner_password_key_vault_secret_name = optional(string)
+    resource_owner_username                       = optional(string)
+    token_endpoint                                = optional(string)
     token_body_parameter = optional(list(object({
       name  = string
       value = string
